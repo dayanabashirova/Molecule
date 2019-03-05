91736c8d00c0905632b276fc727ccf50379bd710
Dayana Bashirova <dayana.bashirova@yandex.ru>
2019-03-05 23:41:17 +0300

    XVI group updated multiplicity fixed for O

--- Molecule/periodictable/XVI.py
+++ Molecule/periodictable/XVI.py
@@ -56,7 +56,9 @@ class O(Element, PeriodI, GroupXVI):
 
     @property
     def valences_exceptions(self):
-        return ((0, 1, ((2, 'O'), (1, 'O')), (0, 2, (1, 'O')), (-1, 2, (1, '0')))
+        return ((0, 2, (1, 'H')), (0, 2, (1, 'N')), (0, 2, (1, 'O')), (-1, 2, (1, 'H')), (-1, 2, (1, 'O')))
+
+            #(0, 1, ((2, 'O'), (1, 'O')), (0, 2, (1, 'O')), (-1, 2, (1, '0')))
 
 
 class S(Element, PeriodII, GroupXVI):
